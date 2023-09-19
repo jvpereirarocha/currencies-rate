009d2791c1b92593786e14f5574192bcde4c8f59
Joao Victor <jvpr.tech@gmail.com>
2023-09-19 08:31:49 -0300

    setting the template of homeview

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,10 +1,56 @@
 <script setup>
+  import { reactive } from 'vue';
+  import Interval from '@/components/Interval.vue'
+  import Chart from '@/components/Chart.vue'
+  
+
+  const chartData = reactive({})
+  const interval = reactive({
+    startDate: '',
+    endDate: ''
+  })
+
 </script>
 
 <template>
   
   <div class="container">
-    <h1>Hello World</h1>
+    <header>
+      <h1>Dashboard</h1>
+      <Interval :startDate="interval.startDate" :endDate="interval.endDate"/>
+    </header>
+    <main>
+      <Chart :startDate="interval.startDate" :endDate="interval.endDate"/>
+    </main>
   </div>
 
 </template>
+
+<style scoped>
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 2rem;
+  width: calc(100% - 4rem);
+  height: calc(100% - 4rem);
+  background-color: #f5f5f5;
+}
+
+h1 {
+  margin-bottom: 4rem;
+  font-size: 2rem;
+  font-weight: 700;
+  color: #363636;
+  text-align: center;
+}
+
+header {
+  margin-bottom: 3rem;
+}
+
+
+</style>
+
