8e4eb36c67ec6ca91bfef051958c5f49c9a43fd0
Joao Victor <jvpr.tech@gmail.com>
2023-09-20 08:02:53 -0300

    changing abstractions from repository

--- backend/domain/abstractions/repository/abstract_currency_rate_repository.py
+++ backend/domain/abstractions/repository/abstract_currency_rate_repository.py
@@ -1,25 +1,19 @@
 from abc import ABC, abstractmethod
-from typing import Set
+from datetime import date
+from typing import Optional, Set
 
 from domain.entities.currency import CurrencyRate
 from domain.abstractions.repository.abstract_repo import AbstractRepository
 
 
 class AbstractCurrencyRateRepository(AbstractRepository):
-    def __init__(
-        self,
-    ):
-        self._entities: Set[CurrencyRate] = set()
 
     @abstractmethod
     def get_currency_rate_by_name_abbreviation_and_date(
-        self, currency: str
-    ) -> CurrencyRate:
+        self, currency: str, date: date
+    ) -> Optional[CurrencyRate]:
         raise NotImplementedError()
 
     @abstractmethod
     def save_currency_rate(self, currency_rate: CurrencyRate) -> None:
         raise NotImplementedError()
-
-    def commit(self):
-        pass
--- backend/domain/abstractions/repository/abstract_repo.py
+++ backend/domain/abstractions/repository/abstract_repo.py
@@ -5,17 +5,4 @@ class AbstractRepository(ABC):
     """
     Base class for repositories.
     """
-
-    @abstractmethod
-    def commit(self):
-        """
-        Commit changes to the repository.
-        """
-        raise NotImplementedError()
-
-    @abstractmethod
-    def rollback(self):
-        """
-        Rollback changes to the repository.
-        """
-        raise NotImplementedError()
+    pass
