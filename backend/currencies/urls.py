6e9c8bff29b5d1dc9d622cf8cb4f4e5ae1d7a082
Joao Victor <jvpr.tech@gmail.com>
2023-09-20 07:03:53 -0300

    setting up url config

--- backend/currencies/urls.py
+++ backend/currencies/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from currencies.views import index, get_currencies_rates
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/v1/currencies/", include("currencies.urls")),
+    path("", index, name="index"),
+    path("get_currencies_rates/", get_currencies_rates, name="get_currencies_rates"),
 ]
